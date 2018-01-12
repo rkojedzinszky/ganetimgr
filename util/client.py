229ef82657bd328c36f75275e0eeb853169f35a3
Richard Kojedzinszky <krichy@nmdps.net>
2018-01-12 10:44:37 +0100

    Support cURL with LibreSSL

--- util/client.py
+++ util/client.py
@@ -309,6 +309,8 @@ def GenericCurlConfig(verbose=False, use_signal=False,
       if capath:
         raise Error("cURL linked against GnuTLS has no support for a"
                     " CA path (%s)" % (pycurl.version, ))
+    elif lcsslver.startswith("libressl/"):
+        pass
     else:
       raise NotImplementedError("cURL uses unsupported SSL version '%s'" %
                                 sslver)
